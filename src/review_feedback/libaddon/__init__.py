c6860c83e13e644f292ed00e60eb00daa4e2ef91
Glutanimate <glutanimate@users.noreply.github.com>
2020-01-28 21:04:24 +0100

    basename → dirname

--- src/review_feedback/libaddon/__init__.py
+++ src/review_feedback/libaddon/__init__.py
@@ -46,8 +46,10 @@ from ._version import __version__  # noqa: F401
 def registerLegacyVendorDir():
     """Some modules like "typing" cannot be properly vendorized, so fall back
     to hacky sys.path modifications if necessary
+    NOTE: make sure not to use vendored legacy dependencies before running this
     """
     import sys
     import os
 
-    sys.path.insert(0, os.path.join(os.path.basename(__file__), "_vendor_legacy"))
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "_vendor_legacy"))
+    print(sys.path)
